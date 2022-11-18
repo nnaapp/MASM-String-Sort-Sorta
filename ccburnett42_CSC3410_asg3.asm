045064c54996b55f869f45d420302e6e66b99389
nnaapp <connor@jnj.org>
2022-11-18 09:20:50 -0600

    Cleaned up code, implemented varying string sizes up to 255 chars

--- ccburnett42_CSC3410_asg3.asm
+++ ccburnett42_CSC3410_asg3.asm
@@ -5,12 +5,10 @@
 ExitProcess PROTO, dwExitCode:DWORD
 
 .data
-s1 DB 'abc', 0
-s2 DB 'bca', 0
-s3 DB 'bac', 0
+s1 DB 'eebus', 0
+s2 DB 'beebus', 0
+s3 DB 'deebus', 0
 string_array DD s1, s2, s3
-array_index DWORD 0
-index_array DW 1, 2, 3
 array_size DWORD 0
 stringorder WORD 0
 tmpcount WORD 0
@@ -25,55 +23,30 @@ ENDM
 
 main PROC
 	XOR eax, eax
-	XOR ebx, ebx
-
-	MOV array_size, LENGTHOF string_array
-
-	LEA esi, string_array
-	MOV array_index, esi
-	MOV esi, [array_index]
-	MOV esi, [esi]
-
-	MOV ecx, 0
-	_loop:
-	CMP array_size, ecx
-	JE _end_loop_dep
-	INC ecx
-
-	PUSH esi
-	CALL strlen
-
-	MOV ebx, [esi]
-
-	ADD array_index, SIZEOF DWORD
-	MOV esi, [array_index]
-	MOV esi, [esi]
-	JMP _loop
-
-	_end_loop_dep:
-
-	LEA ebx, s1
-	LEA edx, s2
-	PUSH ebx
-	PUSH edx
-	CALL a_cmpsb
-
-	XOR eax, eax
 	MOV al, -4
+
 	_sort_loop:
-	MOV tmpcount, 0
 	ADD al, 4
 	MOV ah, 0
+	MOV tmpcount, 0
 
 	_inner_loop:
 	CMP ah, SIZEOF string_array
 	JGE _end_loop
 
 	PUSH eax
+
 	MOVZX ebx, al
-	ADD ebx, string_array
 	MOVZX edx, ah
-	ADD edx, string_array
+
+	LEA eax, string_array
+
+	ADD ebx, eax
+	MOV ebx, [ebx]
+
+	ADD edx, eax
+	MOV edx, [edx]
+
 	PUSH ebx
 	PUSH edx
 	CALL a_cmpsb
@@ -129,28 +102,34 @@ main PROC
 	INVOKE ExitProcess, 0
 main ENDP
 
-strlen PROC ; after ret, EAX contains strlen, ECX contains string ptr from stack
+strlen PROC ; after ret, ESI contains string length, KEEP IT TO 255 OR LESS FOR BYTE SIZE! Store offset value in AH
 	PUSH ebp
 	MOV ebp, esp
-	MOV eax, [esp + 8] ; string ptr
-	DEC eax
+	PUSH eax
+	MOV esi, [ebp + 12] ; string ptr
+	MOV eax, [ebp + 8]  ; offset
+	AND eax, 00000000000000001111111100000000b
+	SHR eax, 8
+	ADD esi, eax
+	DEC esi
 
 	_loop:
-	INC eax
-	CMP BYTE PTR [eax], 0
+	INC esi
+	CMP BYTE PTR [esi], 0
 	JNE _loop
 	
-	SUB eax, [esp]
+	SUB esi, [esp]
+	POP eax
 	POP ebp
-	ret 4
+	ret 8
 strlen ENDP
 
 a_cmpsb PROC ; after ret, ebx contains first string adr, edx contains second string adr, alphabetically
 	PUSH ebp
 	MOV ebp, esp
 
-	MOV esi, [esp + 12] ; str1
-	MOV edi, [esp + 8]  ; str2
+	MOV esi, [ebp + 12] ; str1
+	MOV edi, [ebp + 8]  ; str2
 	PUSH esi
 	PUSH edi
 
@@ -188,44 +167,5 @@ a_cmpsb PROC ; after ret, ebx contains first string adr, edx contains second str
 	POP ebp
 	RET
 a_cmpsb ENDP
-
-;bubblesort PROC	
-;	LOCAL array_addr:DWORD
-;	LOCAL len:DWORD
-;	LOCAL index:DWORD
-;
-;	PUSH ebp
-;	MOV ebp, esp
-;
-;	MOV ebx, [esp + 12]; string array
-;	MOV array_addr, ebx
-;
-;	MOV ecx, [esp + 8] ; array length
-;	DEC ecx
-;	MOV len, ecx
-;	DEC ecx
-;
-;	_outer_loop:
-;	MOV index, 0
-;	MOV edx, ecx
-;
-;	_inner_loop:
-;	MOV esi, ebx
-;	MOV esi, [esi + eax]
-;	MOV edi, ebx
-;	MOV edi, [edi + eax + SIZEOF DWORD]
-;
-;	PUSH ecx
-;	PUSH edx
-;	PUSH esi
-;	PUSH edi
-;	CALL a_cmpsb
-;	POP edx
-;	POP ecx
-;	
-;
-;	POP ebp
-;	RET 8
-;bubblesort ENDP
 
 END main        ;specify the program's entry point
